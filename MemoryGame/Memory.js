83d30cab5cb278cb3ac99c6b74fe9cffded22fb5
Nick van den Berg <vdberg.nj@gmail.com>
2019-10-19 00:44:10 +0200

    Update Memory.js  - Formatted

--- MemoryGame/Memory.js
+++ MemoryGame/Memory.js
@@ -1,6 +1,5 @@
-var points= 0;
-            var moves= 0;
-           
+	var points= 0;
+	var moves= 0;
 	var aangeklickt = [];
 	var img = new Array(15);
 		img[0] = "Images/Banaan.jpg";
@@ -20,7 +19,7 @@ var points= 0;
 		img[14] = "Images/perzik.jpg";
 		img[15] = "Images/perzik.jpg";
 
-//shuffle's the array with parameter array
+	//shuffle's the array with parameter array
 	function shuffleArray() { 
 		for(var i = img.length - 1; i > 0; i--) { 
 			var j = Math.floor(Math.random() * (i + 1));
@@ -30,62 +29,58 @@ var points= 0;
 		}
 		return img;
 	}
+
 	img = shuffleArray(img);
-		console.log(img);
-			shuffleArray();
-//array with images when turned around
-	
-		function tileclick(tileID) { 
-			var x = document.getElementById("tile" + tileID);
+	console.log(img);
+	shuffleArray();
 
-			x.src = img[tileID];
+	//Function: when tile is clicked
+	function tileclick(tileID) { 
+		var x = document.getElementById("tile" + tileID);
+		x.src = img[tileID];
+		aangeklickt.push(tileID);
+		checkTiles();
+	}
 
-			aangeklickt.push(tileID);
-			checkTiles();
-					}
+	//terugdraai functie
+	function turnBack() { 
+		var img1 = document.getElementById("tile" + aangeklickt[0]);
+		var img2 = document.getElementById("tile" + aangeklickt[1]);
+		img1.src = "Images/marley.jpg";
+		img2.src = "Images/marley.jpg";
+		aangeklickt = [];
+	}
 
-					//terugdraai functie
-					function turnBack() { 
-						var img1 = document.getElementById("tile" + aangeklickt[0]);
-						var img2 = document.getElementById("tile" + aangeklickt[1]);
-							img1.src = "Images/marley.jpg";
-							img2.src = "Images/marley.jpg";
-							aangeklickt = [];
-					}
-
-					//function checktiles
-						function checkTiles() { 
-							if (aangeklickt.length == 2) { 
-								if (img[aangeklickt[0]] != img[aangeklickt[1]]) { 
-									setTimeout(function() { 
-										turnBack();
-									},500)	
-								}
-								else { 
-									aangeklickt = [];
-								}
-							}
-						}
-
-						//timer functie
-		setInterval(Timer, 1000)//triggers function every time it hits 100 Ms that's equal to 1 second 
-		 var Time = 0;
-         var TimeMin = 0;
-
-	function Timer() {
-						Time++ // increments +1
-						if (Time > 59) { //here we set the condition if its true, if the condition is true-
-							Time = Time - 60//there will be set a new value because we side -60 and add 1 to the variable timeMin
-							TimeMin++ // increments +1 
-						}
-							if (Time < 10)	{ // here we set another condition if time is less than 10 seconds excute the following 
-							document.getElementById("Time").innerHTML = "Time: " + TimeMin + ":0"+ Time;
-						}
-								else	{
-							document.getElementById("Time").innerHTML = "Time: " + TimeMin + ":"+ Time;
-						}
-					}
-		//reload function 
-		function refresh() { 
-			location.reload();
+	//function checktiles
+	function checkTiles() { 
+		if (aangeklickt.length == 2) { 
+			if (img[aangeklickt[0]] != img[aangeklickt[1]]) { 
+				setTimeout(function() { 
+					turnBack();
+				},500)	
+			} else { 	
+				aangeklickt = [];
+			}
 		}
+	}
+
+	//timer functie
+	 setInterval(Timer, 1000)//triggers function every time it hits 100 Ms that's equal to 1 second 
+	 var Time = 0;
+         var TimeMin = 0;
+	 function Timer() {
+		Time++ // increments +1
+		if (Time > 59) { //here we set the condition if its true, if the condition is true-
+			Time = Time - 60//there will be set a new value because we side -60 and add 1 to the variable timeMin
+			TimeMin++ // increments +1 
+		} else if (Time < 10) { // here we set another condition if time is less than 10 seconds excute the following 
+			document.getElementById("Time").innerHTML = "Time: " + TimeMin + ":0"+ Time;
+		} else {
+			document.getElementById("Time").innerHTML = "Time: " + TimeMin + ":"+ Time;
+		}
+	 }
+	
+	//reload function 
+	function refresh() { 
+		location.reload();
+	}
